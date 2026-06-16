caa3dcdb4e016863270fad412442c913c3344ce3
nnn664121-glitch <nnn664121@gmail.com>
2026-06-16 20:18:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -232,10 +232,8 @@ with tab1:
                 'Skor_Produktivitas': 'Skor Produktivitas (1-5)'
             }
         )
-        fig_box.update_layout(
-            showlegend=False,
-            hovertemplate="<b>%{x} cangkir</b><br>Skor: %{y:.2f}<extra></extra>"
-        )
+        fig_box.update_layout(showlegend=False)
+fig_box.update_traces(hovertemplate="<b>%{x} cangkir</b><br>Skor: %{y:.2f}<extra></extra>")
         st.plotly_chart(fig_box, use_container_width=True)
     
     with col_d:
